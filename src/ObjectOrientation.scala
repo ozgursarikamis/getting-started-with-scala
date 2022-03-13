a465872ea611544a9340cfdd9ab3edf4de2c999a
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-14 01:41:46 +0300

    infix notation

--- src/ObjectOrientation.scala
+++ src/ObjectOrientation.scala
@@ -40,11 +40,22 @@ class ObjectOrientation extends App {
     def eat(a: Animal): Unit
   }
 
+  trait Philosopher {
+    def ?!(thought: String): Unit
+  }
+
   // single-class inheritance, multi-trait mixing...
-  class Crocodile extends Animal with Carnivore {
+  class Crocodile extends Animal with Carnivore with Philosopher {
     override def eat(a: Animal): Unit = println("I am eating you")
 
     // or:
-    override def eat(): Unit = super.eat()
+    // override def eat(): Unit = super.eat()
+
+    override def ?!(thought: String): Unit = println(s"I was thinking $thought")
   }
-}
+
+  val aCroc = new Crocodile
+  aCroc.eat(dog)
+  aCroc eat dog // infix notation for single-parameter methods.
+  aCroc ?! "What if we could fly.."
+ }
