23e9b98cab30695bbda28714c1dc9d55f20dcd93
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-14 01:14:54 +0300

    Class definition

---
+++ src/ObjectOrientation.scala
@@ -0,0 +1,15 @@
+package com.zanatech
+
+class ObjectOrientation extends App {
+  // class declaration
+  class Animal {
+
+    // class members
+
+    // fields
+    val age: Int = 0
+
+    // methods:
+    def eat(): Unit = println("Eating...")
+  }
+}
