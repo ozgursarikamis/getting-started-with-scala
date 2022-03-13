3d32c14e3ad60a0e6252d221ee8dc4d72613b6d0
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-14 00:45:24 +0300

    if expression and chained if expression

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -3,4 +3,14 @@ package com.zanatech
 object HelloWorld extends App {
   val meaningOfLife: String = "Hello Scala World"
   println(meaningOfLife)
+
+  val ifExpress = if (meaningOfLife.length > 10) "long sentence" else "this is short"
+  println(ifExpress)
+
+  val chainedIfExpress = if (meaningOfLife.length > 100) "long sentence"
+    else if (meaningOfLife.length < 0) "It's negative?"
+    else if (meaningOfLife.isEmpty) "there's no sentence"
+    else "meaningless"
+
+  println(chainedIfExpress)
 }
