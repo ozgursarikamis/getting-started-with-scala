0007104f15eb39ea81cd01fa53fe5da6cfb14ee3
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-19 03:45:38 +0300

    fields and values

--- src/HelloScala.scala
+++ src/HelloScala.scala
@@ -2,13 +2,26 @@ package com.zanatech
 
 object HelloScala extends App {
 
-  class Employee(firstName: String, lastName: String) {
-    override def toString: String = firstName + " " + lastName
+  class Employee(firstName: String, lastName: String, s: Int) {
+
+    // Fields
+    private val first: String = ""
+    private val last: String = ""
+    private var stocks: Int = s
+
+    def getFirst: String = first
+    def getLast: String = last
+    def getStocks: Int = stocks
+
+    def awardMoreStocks(numberOfStocks: Int): Unit = stocks += numberOfStocks
+
+    override def toString: String = firstName + " " + lastName + " " + stocks
   }
 
-  val bob = new Employee("Bob", "Martin")
-  println(bob.toString)
-
-  //  println(bob.firstName)
-  // value firstName is not a member of com.zanatech.HelloScala.Employee
+  val bob = new Employee("Bob", "Martin", 10)
+  println(bob.getFirst)
+  println(bob.getLast)
+  println(bob.getStocks)
+  val stockAfterAward: Unit = bob.awardMoreStocks(100)
+  println(bob.getStocks, stockAfterAward)
 }
