1c2c5b48343da708ac59dab837f644de0ba08b5f
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-24 01:06:39 +0300

    First definition

--- src/HelloScala.scala
+++ src/HelloScala.scala
@@ -1,3 +1,20 @@
 object HelloScala extends App {
   println("Hello Scala World")
 }
+
+object ParameterizedPair {
+  case class Pair[T, U](fst: T, sec: U) {
+    override def toString: String = s"==++ $fst, $sec ++=="
+
+    def switch: Pair[U, T] = this.copy(fst=sec, sec = fst)
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val intStringPair: Pair[Int, String] = Pair(1, "First")
+    val switched = intStringPair.switch
+
+    println(s"IntStringPair $intStringPair | Switched: $switched")
+
+  }
+}
