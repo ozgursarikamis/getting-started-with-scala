9870e52d193864f6c8ba2bf95cd19f761db59a20
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-25 01:21:45 +0300

    updating class members

--- src/AccountRunner.scala
+++ src/AccountRunner.scala
@@ -4,9 +4,9 @@ import java.util.UUID
 object AccountRunner extends App {
 
   class Account(id: UUID, name: String, dateOpened: LocalDateTime) {
-    var _id: UUID = id
-    var _name: String = name
-    var _dateOpened: LocalDateTime = dateOpened
+    val _id: UUID = id
+    val _name: String = name
+    val _dateOpened: LocalDateTime = dateOpened
 
     def this(name: String) {
       this(UUID.randomUUID(), name, LocalDateTime.now())
@@ -25,8 +25,8 @@ object AccountRunner extends App {
 
   println(a3._id, a3._name, a3._dateOpened)
 
-  a1._name = "Updated Name"
-  a1._id = UUID.randomUUID() // not a safe way
+  //  a1._name = "Updated Name"   // Does not compile, the class member is decorated with val now.
+  //  a1._id = UUID.randomUUID()  // Does not compile, the class member is decorated with val now.
 
   println()
   println(a1._name, a1._id)
