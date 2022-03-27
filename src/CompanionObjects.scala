c35693472609a41f1dc62dcbd4020512cf244ac6
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-27 03:20:31 +0300

    Companion Objects: definition

---
+++ src/CompanionObjects.scala
@@ -0,0 +1,9 @@
+object CompanionObjects extends App {
+
+  /*
+  * A companion object in Scala is an object that’s declared in the same file as a class,
+  * and has the same name as the class.
+  */
+  class Pizza {}
+  object Pizza {}
+}
