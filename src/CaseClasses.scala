15c6c6a22072d19d07b62bf72671ece4a6f7d14b
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-27 14:44:10 +0300

    Case Classes: Usage

--- src/CaseClasses.scala
+++ src/CaseClasses.scala
@@ -12,4 +12,29 @@ object CaseClasses extends App {
 
   //  But, mutator methods are not generated:
   //  christina.name = "Fred" // does not compile.
+
+
+  //  A great thing about a case class is that it automatically generates an unapply
+  //  method for your class, so you don’t have to write one.
+
+  trait Person2 { def name: String }
+
+  case class Student(name: String, year: Int) extends Person2
+  case class Teacher(name: String, specialize: String) extends Person2
+
+  def getPrintableString(p: Person2): String = p match {
+    case Student(name, year) => s"$name is a student in Year $year"
+    case Teacher(name, whatTheyTeach) => s"$name teaches $whatTheyTeach"
+  }
+
+  val s = Student("A1", 1)
+  val t = Teacher("Bob Donnan", "Mathematics")
+
+  println()
+  val result = getPrintableString(s)
+  println(result)
+
+  val result2 = getPrintableString(t)
+  println(result2)
+
 }
