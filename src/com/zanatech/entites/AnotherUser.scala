baaba008a02acd94639a78822764b3ad08bc021c
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-03-27 14:50:25 +0300

    importing from other packages

--- src/com/zanatech/entites/AnotherUser.scala
+++ src/com/zanatech/entites/AnotherUser.scala
@@ -1,7 +1,16 @@
 package com {
   package zanatech {
     package entites {
-      case class AnotherUser()
+      case class AnotherUser(f: String, l:String)
     }
+  }
+}
+
+package main {
+
+  import com.zanatech.entites.AnotherUser
+
+  class Runner extends App {
+    val u: AnotherUser = AnotherUser("Tim", "Cook")
   }
 }
