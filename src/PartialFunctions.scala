7af389fc97df72020e285bfa01431ae7ea3cf5d8
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-29 14:20:05 +0300

    Function: PartialFunction

---
+++ src/PartialFunctions.scala
@@ -0,0 +1,22 @@
+object PartialFunctions {
+  def main(args: Array[String]): Unit = {
+    // val divide64By = (x: Int) => 64 / x
+    // println(divide64By(8))
+    // println(divide64By(0))
+
+    val divide64By = new PartialFunction[Int, Int] {
+
+      def isDefinedAt(x: Int): Boolean = x != 0
+
+      def apply(x: Int): Int = 64 / x
+
+    }
+
+    println("Is Defined At 0: " + divide64By.isDefinedAt(0))
+    println("Is Defined At 11: " + divide64By.isDefinedAt(11))
+    // println("Defined at 11: " + divide64By(11))
+    // println("Defined at 0 : " + divide64By(0))
+
+    println("Divide 64 by 3: " + (if (divide64By.isDefinedAt(3)) divide64By(3)))
+  }
+}
